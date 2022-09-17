09ff8f643c3cffef0a8181e7feeac5cd5cccaab6
unknown <sifatniloy18@gmail.com>
2022-09-16 21:54:19 -0600

    filter used

--- map.js
+++ map.js
@@ -1,3 +1,9 @@
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const result = array.map(a => a * 2)
 console.log(...result);
+
+const result2 = array.find(a => a > 5);
+console.log(result2)
+
+const result3 = array.filter(a => a > 6)
+console.log(result3)
