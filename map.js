67d2cc87f1fe38d105ab13dff031bdcb30fd0deb
unknown <sifatniloy18@gmail.com>
2022-10-05 00:24:18 -0600

    date

--- map.js
+++ map.js
@@ -1,12 +1,14 @@
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const result = array.map(a => a * 2)
-console.log(...result);
+// const result = array.map(a => a * 2)
+// console.log(...result);
 
-const result2 = array.find(a => a > 5);
-console.log(result2)
+// const result2 = array.find(a => a > 5);
+// console.log(result2)
 
-const result3 = array.filter(a => a > 6)
-console.log(result3)
+// const result3 = array.filter(a => a > 6)
+// console.log(result3)
 
-const square = array.map(number => number * number)
-console.log(square)
+// const square = array.map(number => number * number)
+// console.log(square)
+const d = new Date();
+console.log(d)
